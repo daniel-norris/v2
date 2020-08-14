a78a47b0204b7210598cd3b54a2e8d8f137a0b5c
Daniel Norris <dan.norris@hotmail.com>
2020-08-14 17:57:52 +0100

    created a dark mode button

--- src/components/header.js
+++ src/components/header.js
@@ -14,15 +14,17 @@ const Header = () => {
         <header className="flex items-center justify-between">
 
             <Logo text={siteShortTitle} />
-            <nav>
+            <nav className="flex items-center">
                 {menu.map(({ name, url }, key) => {
                     return (
-                        <Link className="text-sm font-bold p-3 rounded hover:bg-gray-200" key={key} to={url}>
+                        <Link className="text-sm font-bold px-3 py-2 rounded hover:bg-gray-200" key={key} to={url}>
                             {name}
                         </Link>
                     )
                 })}
-
+                <button className="hover:bg-gray-400 bg-gray-200 rounded w-8 h-8 p-1">
+                    <Icon name="moon" />
+                </button>
             </nav>
 
         </header >
