0c10c516e02ba84981e781803ba5fc8ecc564958
Daniel Norris <dan.norris@hotmail.com>
2020-08-15 13:14:50 +0100

    updated layout for testimonial

--- src/components/sections/testimonial.js
+++ src/components/sections/testimonial.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Quote from '../../components/quote';
 import Icon from '../../components/icons/index';
+import { Link } from 'gatsby';
 
 const Testimonial = ({ content }) => {
 
@@ -9,7 +10,7 @@ const Testimonial = ({ content }) => {
     return (
         <section id="testimonial" className="mt-6 flex-col" style={{ height: "60vh" }}>
             {/* https://medium.com/modex/css-tricks-expanding-beyond-a-parent-div-10d7b7204c21 */}
-            <div className="grid grid-cols-3 gap-6" style={{ width: "300%", marginLeft: "-100%" }}>
+            <div className="grid grid-cols-3 gap-6" style={{ width: "300%", marginLeft: "-80%" }}>
                 <Quote
                     text={frontmatter.firstTestimonial}
                     referee={frontmatter.firstReferee}
@@ -24,8 +25,13 @@ const Testimonial = ({ content }) => {
                     title={frontmatter.thirdTitle} />
             </div>
 
-            <div>
-                <Icon name="arrow-left" />
+            <div className="flex mt-6 ml-8 justify-center">
+                <Link to="/" className="mr-4">
+                    <Icon name="arrow-left" color="#4299e1" />
+                </Link>
+                <Link to="/">
+                    <Icon name="arrow-right" color="#4299e1" />
+                </Link>
             </div>
         </section>
     );
