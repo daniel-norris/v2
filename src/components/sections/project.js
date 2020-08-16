120f4f7046dcbb825785076141cb5673e0a375d3
Daniel Norris <dan.norris@hotmail.com>
2020-08-16 12:38:33 +0100

    changed styling for blog page

--- src/components/sections/project.js
+++ src/components/sections/project.js
@@ -42,10 +42,10 @@ const Project = ({ content }) => {
                                     </div>
                                     <div className="flex mt-4">
                                         <a href={frontmatter.github} className="w-8 h-8 mr-4">
-                                            <Icon name="github" />
+                                            <Icon name="github" color="gray" />
                                         </a>
                                         <a href={frontmatter.external} className="w-8 h-8">
-                                            <Icon name="external" />
+                                            <Icon name="external" color="gray" />
                                         </a>
                                     </div>
                                 </div>
