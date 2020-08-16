b8a35c19f54e7b6bb6f80e0e577fcb21bb15a734
Daniel Norris <dan.norris@hotmail.com>
2020-08-16 14:00:24 +0100

    added additional styling to pre tag and line numbers

--- src/components/code.js
+++ src/components/code.js
@@ -2,6 +2,18 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 
+const lineNo = (i) => {
+    return (
+        <span style={{
+            fontFamily: 'Courier New, Courier, monospace',
+            width: '2rem',
+            userSelect: 'none',
+            opacity: '0.3',
+            marginRight: '1rem',
+        }}>{i + 1}</span>
+    )
+};
+
 const Code = ({ codeString, language }) => {
 
     return (
@@ -19,9 +31,10 @@ const Code = ({ codeString, language }) => {
                 getLineProps,
                 getTokenProps,
             }) => (
-                    <pre className={className} style={style}>
+                    <pre className={`${className} p-4 rounded mb-4`} style={style}>
                         {tokens.map((line, i) => (
                             <div {...getLineProps({ line, key: i })}>
+                                {lineNo(i)}
                                 {line.map((token, key) => (
                                     <span {...getTokenProps({ token, key })} />
                                 ))}
