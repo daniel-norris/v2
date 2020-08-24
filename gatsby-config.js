6a7bd41acde6f1bd55745756b908528afc8f4359
Daniel Norris <dan.norris@hotmail.com>
2020-08-24 12:08:38 +0100

    fixed code block overflow bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,59 +1,58 @@
-const config = require('./src/config/index');
+const config = require("./src/config/index")
 
 module.exports = {
-
-    siteMetadata: {
-        title: config.siteTitle,
-        description: config.siteDescription,
-        author: config.author,
-        url: config.siteUrl,
+  siteMetadata: {
+    title: config.siteTitle,
+    description: config.siteDescription,
+    author: config.author,
+    url: config.siteUrl,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-175596724-1",
+      },
     },
-    plugins: [
-        `gatsby-plugin-postcss`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-google-analytics`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                trackingId: "UA-175596724-1",
+              maxWidth: 590,
             },
-        },
-        {
-            resolve: `gatsby-plugin-mdx`,
+          },
+        ],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                extensions: [`.mdx`, `.md`],
-                gatsbyRemarkPlugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                ],
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                ],
+              maxWidth: 590,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/content`,
-                name: `content`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/content/posts`,
-                name: `posts`,
-            },
-        },
-    ],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content`,
+        name: `content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content/posts`,
+        name: `posts`,
+      },
+    },
+  ],
 }
